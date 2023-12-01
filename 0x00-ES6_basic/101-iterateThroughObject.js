932123bcd5eb35aa95285b02b788b1de0a6d2b86
olisabelema <olisabelema@gmail.com>
2023-12-01 10:04:17 +0000

    updated

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,13 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let employeeNames = '';
-
-  for (const department in reportWithIterator.allEmployees) {
-      employeeNames += reportWithIterator.allEmployees[department].join(' | ') + ' | ';
-  }
-
-  // Remove the trailing ' | ' at the end
-  employeeNames = employeeNames.slice(0, -3);
-
-  return employeeNames;
+  return reportWithIterator.join(' | ');
 }
-
